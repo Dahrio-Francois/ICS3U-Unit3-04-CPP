1d8ebdb0b2f01c35b58a2928714f76c689338e6f
Ubuntu <ubuntu@ip-172-31-39-151.ec2.internal>
2020-12-10 17:45:16 +0000

    added in integers.cpp

---
+++ integers.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) St. Mother Teresa HS All rights reserved
+//
+// Created by: Dahrio Francois
+// Created on: December 2020
+// this program identifies a positive or negative number
+//     with user input
+
+#include <iostream>
+
+
+int main() {
+    // this function identifies a positive or negative number
+    int integer;
+    int number;
+    integer = 0
+    // input
+    ; std::cout << "Enter your number value: ";
+    std::cin >> number;
+
+    // process
+    if (number > integer) {
+        std::cout << " + ";
+
+    } else if (number < integer) {
+        std::cout << " - ";
+
+    } else {
+        ; std::cout << " 0 ";
+}
+}
